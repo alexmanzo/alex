ac1ca33de8c2634cb6b9f8be710225f00dee50c9
Alex Manzo <manzoalex17@gmail.com>
2021-10-23 19:07:15 -0400

    Customize fonts in Tailwind.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,10 @@ const config = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
+    fontFamily: {
+      serif: ['FrauncesVariable', 'ui-serif', 'Georgia'],
+      mono: ['Fira Mono', 'ui-monospace', 'Roboto'],
+    },
     extend: {},
   },
 
